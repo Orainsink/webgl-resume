dc54a91b78084106343f8d1876f566fc8a81cc24
Orainsink <ywt1250066597@gmail.com>
2019-10-22 11:02:54 +0800

    :bug:fix bugs in help module,reconstruct code with ES6 Class and arraw function

--- app/src/js/objects2D/LayoutObject2D.js
+++ app/src/js/objects2D/LayoutObject2D.js
@@ -7,7 +7,148 @@
  * @constructor
  * @requires jQuery
  */
-function Layout($el) {
+
+class Layout {
+  constructor($el) {
+    this.$el = $el;
+
+    this.$container = this.$el.find(".layout__parts");
+    this.$mouse = this.$el.find(".layout__mouse");
+    this.$click = this.$mouse.find(".layout__mouse__click");
+
+    // targets
+    this.y = 0;
+    this.openY = -15;
+    this.mouseY = 90;
+
+    this.interval = null;
+  }
+  /**
+   * Animation next step
+   *
+   * @method slide
+   */
+  slide() {
+    // update targets
+    if (this.y === 0) {
+      this.y = -100;
+      this.openY = -15;
+      this.mouseY = 83;
+    } else {
+      this.y = 0;
+      this.openY = -85;
+      this.mouseY = 3;
+    }
+
+    const open = () => {
+      this.$container.animate(
+        {
+          top: this.openY + "%"
+        },
+        800,
+        function() {
+          click();
+        }
+      );
+    };
+
+    const moveMouse = () => {
+      var flag = false;
+
+      this.$mouse.animate(
+        {
+          top: this.mouseY + "%"
+        },
+        {
+          duration: 500,
+          progress: function(animation, progress) {
+            if (!flag && progress > 0.5) {
+              flag = !flag;
+              open();
+            }
+          }
+        }
+      );
+    };
+
+    const click = () => {
+      var flag = false;
+      this.$click.delay(500).animate(
+        {
+          width: 70,
+          height: 70,
+          "margin-left": -35,
+          "margin-top": -35,
+          opacity: 0
+        },
+        {
+          duration: 400,
+          progress: function(animation, progress) {
+            if (!flag && progress > 0.7) {
+              flag = !flag;
+              slide();
+            }
+          },
+          complete: () => {
+            this.$click.css({
+              width: 0,
+              height: 0,
+              "margin-left": 0,
+              "margin-top": 0,
+              opacity: 1
+            });
+          }
+        }
+      );
+    };
+
+    const slide = () => {
+      this.$container.animate(
+        {
+          top: this.y + "%"
+        },
+        500
+      );
+
+      centerMouse();
+    };
+
+    const centerMouse = () => {
+      this.$mouse.delay(300).animate(
+        {
+          top: "45%"
+        },
+        500
+      );
+    };
+
+    moveMouse();
+  }
+
+  /**
+   * Start animation
+   *
+   * @method start
+   */
+  start() {
+    this.slide();
+
+    this.interval = window.setInterval(() => {
+      this.slide();
+    }, 4000);
+  }
+
+  /**
+   * Stop animation
+   *
+   * @method stop
+   */
+  stop() {
+    window.clearInterval(this.interval);
+  }
+}
+
+/*function Layout($el) {
   this.$el = $el;
 
   this.$container = this.$el.find(".layout__parts");
@@ -20,13 +161,13 @@ function Layout($el) {
   this.mouseY = 90;
 
   this.interval = null;
-}
+}*/
 
-/**
+/*/!**
  * Animation next step
  *
  * @method slide
- */
+ *!/
 Layout.prototype.slide = function() {
   // update targets
   if (this.y === 0) {
@@ -72,7 +213,6 @@ Layout.prototype.slide = function() {
 
   var click = function() {
     var flag = false;
-
     this.$click.delay(500).animate(
       {
         width: 70,
@@ -127,11 +267,11 @@ Layout.prototype.slide = function() {
   moveMouse();
 };
 
-/**
+/!**
  * Start animation
  *
  * @method start
- */
+ *!/
 Layout.prototype.start = function() {
   this.slide();
 
@@ -143,13 +283,13 @@ Layout.prototype.start = function() {
   );
 };
 
-/**
+/!**
  * Stop animation
  *
  * @method stop
- */
+ *!/
 Layout.prototype.stop = function() {
   window.clearInterval(this.interval);
-};
+};*/
 
 export default Layout;
